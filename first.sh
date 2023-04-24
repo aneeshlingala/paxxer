9aecbfe943d3ba60b6784c7a2cfe953f800dc4db
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-04-24 12:32:39 -0700

    Update first.sh

--- first.sh
+++ first.sh
@@ -50,6 +50,7 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
+sudo apt remove firefox firefox-esr
 echo "Installing doas and removing sudo..."
 sudo passwd
 sudo apt install doas
