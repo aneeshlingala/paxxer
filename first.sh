2f82339c1b41deab5792eb6948f7bb04480cd1ad
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-04-24 14:29:49 -0700

    Update first.sh

--- first.sh
+++ first.sh
@@ -50,7 +50,7 @@ sudo apt install qml-module-qtquick-controls qml-module-qtgraphicaleffects -y
 cd ~
 echo "Installing Brave Browser..."
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
 sudo apt remove firefox firefox-esr
