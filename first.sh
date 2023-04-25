7d561267a229fa8af2d09e3fa53df4142a237be8
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-04-25 12:16:02 -0700

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,6 +1,6 @@
 if [[ $EUID -eq 0 ]]; then
 echo "Error: Do not run this script as root!" 1>&2
-exit 1
+echo "Solution: Please run it as a normal user without sudo."
 fi
 echo "Paxxer, a setup tool to setup my Debian system, to my liking."
 echo "Supported Devices: Kukui Mediatek MT8183 ARM64 Chromebooks"
