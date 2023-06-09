3bf06bc99b0255093a46d7aa4eb8f035a31afc04
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-06-08 19:30:27 -0700

    Update second.sh

--- second.sh
+++ second.sh
@@ -3,7 +3,7 @@ sleep 3
 nmtui
 cd /home/aneesh/paxxer
 echo "Setting GTK, Icon, Sound, and Cursor theme..."
-xfconf-query -c xsettings -p /Net/ThemeName -s "Nordic"
+xfconf-query -c xsettings -p /Net/ThemeName -s "Juno"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Zafiro-Icons-Dark"
 xfconf-query --channel xsettings --property /Gtk/CursorThemeName --set oreo_teal_cursors
 xfconf-query -c xsettings -p /Net/EnableEventSounds -s true
@@ -14,7 +14,6 @@ xfconf-query -c xfwm4 -p /general/button_layout -s "CMH|O"
 echo "Cleaning up..."
 cd ~
 rm -rf /home/aneesh/paxxer
-rm -rf /home/aneesh/.config/autostart/secondrun.desktop
 echo "Deleting user linux"
 sudo userdel linux
 sudo rm -rf /home/linux
