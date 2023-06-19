5304ab139f9480c44b42a9c79bf9342eda4ef8b2
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-06-19 16:59:20 -0700

    remove unneccessary line

--- first.sh
+++ first.sh
@@ -33,7 +33,6 @@ sudo apt install git wget curl alsa-utils fish engrampa w3m fonts-noto-color-emo
 sudo apt purge firefox-esr --autoremove -y
 sudo apt remove xarchiver --autoremove -y
 echo "Installing Brave Browser..."
-sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
