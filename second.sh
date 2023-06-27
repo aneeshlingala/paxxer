04e362835a924c656e74d18f55b24681269c4fbd
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-06-26 20:29:51 -0700

    Update second.sh

--- second.sh
+++ second.sh
@@ -17,21 +17,11 @@ rm -rf /home/aneesh/paxxer
 echo "Deleting user linux"
 sudo userdel linux
 sudo rm -rf /home/linux
-echo "Setting GTK, Icon, Sound, and Cursor theme..."
-xfconf-query -c xsettings -p /Net/ThemeName -s "Juno"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "beautyline"
-xfconf-query --channel xsettings --property /Gtk/CursorThemeName --set oreo_teal_cursors
-xfconf-query -c xsettings -p /Net/EnableEventSounds -s true
-xfconf-query -c xsettings -p /Net/EnableInputFeedbackSounds -s true
-xfconf-query -c xsettings -p /Net/SoundThemeName -s "Smooth"
-xfconf-query -c xfwm4 -p /general/theme -s Juno
-xfconf-query -c xfwm4 -p /general/button_layout -s "CMH|O"
 echo "Adding some things to autostart..."
 cd /home/aneesh/paxxer
 mkdir ~/.config
 mkdir ~/.config/autostart
 cp conky.desktop ~/.config/autostart/
-cp startsound.desktop ~/.config/autostart/
 echo "Updating System..."
 sudo apt upgrade -y
 echo "PLEASE READ below:"
