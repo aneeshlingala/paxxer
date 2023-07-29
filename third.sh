85e301dc0037baaf79b73dfffbe7866d3e1969a0
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-07-29 16:47:34 -0700

    Update third.sh

--- third.sh
+++ third.sh
@@ -1,3 +1,17 @@
+if [ "$(whoami)" != "aneesh" ]; then
+        echo "Error: Please reboot and run the script as user aneesh."
+        echo "Not rebooting but instead logging out and logging in as aneesh will cause the script to break."
+        exit
+fi
+
+if [[ -f "/etc/paxxer-second" ]]; then
+    echo "Second.sh has been run, good."
+    sudo rm -rf /etc/paxxer-second
+else
+    echo "Error: Please run second.sh, then third.sh."
+    exit
+fi
+
 echo "Upgrading to Debian Unstable..."
 sudo rm -rf /etc/apt/sources.list
 sudo touch /etc/apt/sources.list
@@ -5,3 +19,4 @@ echo "deb http://deb.debian.org/debian/ sid main contrib non-free non-free-firmw
 sudo apt update -y
 echo "Note: Please check if apt is removing any necessary packages."
 sudo apt full-upgrade --autoremove
+sudo touch /etc/paxxer-successful
