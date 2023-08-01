27cfe703fd59b5af4d2cd4bc6c390771b4626657
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-08-01 13:21:29 -0700

    Update first.sh

--- first.sh
+++ first.sh
@@ -115,10 +115,11 @@ echo "[SeatDefaults]" | sudo tee -a /etc/lightdm/lightdm.conf
 echo "greeter-session=slick-greeter" | sudo tee -a /etc/lightdm/lightdm.conf
 sudo rm -rf /etc/lightdm/slick-greeter.conf
 sudo touch /etc/lightdm/slick-greeter.conf
-echo "background=#008080" | sudo tee -a /etc/lightdm/slick-greeter.conf 
-echo "background-color=#008080" | sudo tee -a /etc/lightdm/slick-greeter.conf 
+echo "background=/usr/share/wallpapers/SpaceFun/contents/images/1920x1080.svg" | sudo tee -a /etc/lightdm/slick-greeter.conf 
 echo "draw-user-backgrounds=true" | sudo tee -a /etc/lightdm/slick-greeter.conf
 echo "onscreen-keyboard=false" | sudo tee -a /etc/lightdm/slick-greeter.conf
+echo "theme-name=Juno" | sudo tee -a /etc/lightdm/slick-greeter.conf
+echo "icon-theme-name=Fluent" | sudo tee -a /etc/lightdm/slick-greeter.conf
 echo "Installing Wallpapers..."
 sudo mkdir /usr/share/backgrounds
 sudo mkdir /usr/share/backgrounds/debian-lights
