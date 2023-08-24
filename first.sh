102f62cac9051f846b15a7c1d4fc4132ba371cb9
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-08-24 09:43:25 -0700

    remove old debian bullseye support
    debian bullseye images are outdated and not recommended.
    so, removing it.

--- first.sh
+++ first.sh
@@ -50,7 +50,7 @@ else
 fi
 
 echo "Paxxer, a setup tool to setup my Debian system, to my liking."
-echo "Version: 2023.08.23"
+echo "Version: 2023.08.24"
 
 echo "Extending rootfs to max and increasing swapfile..."
 sudo bash /scripts/extend-rootfs.sh
@@ -115,34 +115,7 @@ sudo ln -s /usr/share/wallpapers/SpaceFun/contents/images/1920x1080.svg /etc/lig
 cd ~
 echo "Setting default shell as fish..."
 sudo chsh --shell /usr/bin/fish aneesh
-
-if [ "$KERNEL" = "5.19.1-stb-mt8+" ]
-then
-    echo "Updating Kernel..."
-    sudo rm -rf /boot/*
-    sudo rm -rf /lib/modules/*
-    cd ~
-    sudo wget "https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/6.1.11-stb-mt8%2B/6.1.11-stb-mt8+.tar.gz"
-    sudo tar -xzvf 6.1.11-stb-mt8+.tar.gz -C / 
-    sudo rm -rf 6.1.11-stb-mt8+.tar.gz
-    read -p "Enter the disk node (e.g., sda, sdb, etc.): " disk_node
-
-    if [[ $disk_node =~ mmcblk[01] ]]; then
-        echo "Running different command for $disk_node..."
-        sudo dd if=/boot/vmlinux.kpart-6.1.11-stb-mt8+ of=/dev/${disk_node}p1
-        sudo dd if=/boot/vmlinux.kpart-6.1.11-stb-mt8+ of=/dev/${disk_node}p2
-        sudo update-initramfs -u
-
-    elif [[ -b $disk_node ]]; then
-        sudo dd if=/boot/vmlinux.kpart-6.1.11-stb-mt8+ of=/dev/${disk_node}1
-        sudo dd if=/boot/vmlinux.kpart-6.1.11-stb-mt8+ of=/dev/${disk_node}2
-        sudo update-initramfs -u
-    else
-        echo "Invalid disk node. Please enter a valid disk node."
-        exit
-    fi
-fi
-
+echo "Copying files for second.sh..."
 sudo mkdir /home/aneesh/paxxer
 sudo cp $PAXXERDIR/second.sh /home/aneesh/paxxer
 sudo cp $PAXXERDIR/.conkyrc /home/aneesh/paxxer
