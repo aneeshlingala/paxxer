6dac70317bb0c46b3f9e47b816fadcc1c5301ec0
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-08-26 10:23:30 -0700

    Update first.sh

--- first.sh
+++ first.sh
@@ -149,6 +149,11 @@ sudo cp $PAXXERDIR/.conkyrc /home/aneesh/paxxer
 sudo cp $PAXXERDIR/conky-startup.desktop /home/aneesh/paxxer
 sudo chmod +x /home/aneesh/paxxer/second.sh
 sudo touch /etc/paxxer-first-done
+
+if [[ "$ARCH" == "x86_64" ]]; then
+    echo $USER | sudo tee -a /etc/paxxer-user
+fi
+
 echo $USER | sudo tee -a /etc/paxxer-user
 echo "After rebooting, run the second.sh script in /home/aneesh/paxxer."
 echo "NOTE: After rebooting, login as user aneesh, or the script will break."
