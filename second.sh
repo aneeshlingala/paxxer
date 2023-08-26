8a61fad1eb56e75c8f74e8a6b425d7477290feea
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-08-26 14:21:00 -0700

    Update second.sh

--- second.sh
+++ second.sh
@@ -87,7 +87,7 @@ sudo touch /etc/paxxer-second
 sudo rm -rf /home/aneesh/paxxer
 sudo mkdir /home/aneesh/paxxer
 cd /home/aneesh/paxxer
-echo $(uname -r) | sudo tee -a /etc/paxxer-kernel
+uname -r | sudo tee -a /etc/paxxer-kernel
 sudo wget https://raw.githubusercontent.com/aneeshlingala/paxxer/main/third.sh
 sudo chmod +x third.sh
 cd ~
