2bdd08680c66c51f13c7db60005df50b5bae85fb
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-09-09 17:05:36 -0700

    Update first-archlinux.sh

--- first-archlinux.sh
+++ first-archlinux.sh
@@ -56,6 +56,7 @@ echo "Version: 2023.09.09"
 
 echo "Adding user Aneesh..."
 sudo useradd aneesh
+sudo mkdir /home/aneesh
 sudo usermod -a -G wheel aneesh
 echo "Setting root password..."
 sudo passwd
