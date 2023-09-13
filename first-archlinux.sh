d425cd8d90656610c62b19378eb0367a30aa0cda
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-09-12 19:08:14 -0700

    last updated 9/12/2023

--- first-archlinux.sh
+++ first-archlinux.sh
@@ -52,7 +52,7 @@ else
 fi
 
 echo "PaxxerAL, a setup tool to setup my Arch Linux system, to my liking."
-echo "Version: 2023.09.09"
+echo "Version: 2023.09.12"
 
 echo "Adding user Aneesh..."
 sudo useradd aneesh
