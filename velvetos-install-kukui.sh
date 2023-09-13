eb528711562d0e414681c2f965a2d93464ed8004
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-09-13 23:08:12 +0000

    add checks

--- velvetos-install-kukui.sh
+++ velvetos-install-kukui.sh
@@ -1,3 +1,32 @@
+ARCH=$(uname -m)
+echo "Running checks..."
+
+if [[ $EUID -eq 0 ]]; then
+echo "Error: Do not run this script as root!" 1>&2
+echo "Solution: Run this script as a normal user without sudo."
+exit
+fi
+
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+  echo "You are online, continuing..."
+  echo ""
+else
+  echo "Error: You are offline."
+  echo ""
+  echo "Press any key to launch the Network Connection Wizard..."
+  read -s -n 1
+  echo ""
+  echo "Pressed a key, launching the Network Connection Wizard..."
+  nmtui
+fi
+
+if [[ "$ARCH" == "aarch64" ]]; then
+    echo "Architecture is aarch64, continuing..."
+else
+    echo "The VelvetOS installer only works on the aarch64 architecture"
+    exit
+fi
+
 echo "VelvetOS Installer"
 echo "Version 2023.09.13"
 echo "NOTE: Make sure you have run /scripts/extend-rootfs.sh for more space"
