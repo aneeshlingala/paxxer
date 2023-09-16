64ce44e19c613c5aed9badd11e88fe1dbed48483
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-09-16 16:58:18 +0000

    Update first.sh

--- first.sh
+++ first.sh
@@ -59,7 +59,7 @@ then
 fi
 
 echo "PaxxerDeb, a setup tool to setup my Debian system, to my liking."
-echo "Version: 2023.09.13"
+echo "Version: 2023.09.16"
 
 if [[ "$ARCH" == "aarch64" ]]; then
     echo "Extending rootfs to max and increasing swapfile..."
