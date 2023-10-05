27b60854b07e73f16e2bb3d49fe4d0d7917a4f23
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-10-04 17:24:34 -0700

    new kernel

--- first.sh
+++ first.sh
@@ -89,16 +89,16 @@ if [[ "$ARCH" == "aarch64" ]]; then
     sudo hostname kappa
     sudo sed -i "s/$CUR_HOSTNAME/kappa/g" /etc/hosts
     sudo sed -i "s/$CUR_HOSTNAME/kappa/g" /etc/hostname
-    echo "Updating kernel from $(echo $KERNEL) to 6.1.51-stb-mt8+"
+    echo "Updating kernel from $(echo $KERNEL) to 6.4.7-stb-mt8"
     sudo rm -rf /boot/*
     sudo rm -rf /lib/modules/*
     cd ~
-    sudo wget "https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/6.1.51-stb-mt8%2B/6.1.51-stb-mt8+.tar.gz"
+    sudo wget "https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/6.4.7-stb-mt8%2B/6.4.7-stb-mt8+.tar.gz"
     cd /
-    sudo tar -xzvf ~/6.1.51-stb-mt8+.tar.gz
-    sudo rm -rf 6.1.51-stb-mt8+.tar.gz
-    sudo dd if=/boot/vmlinux.kpart-6.1.51-stb-mt8+ of=/dev/mmcblk0p1 bs=1M status=progress
-    sudo dd if=/boot/vmlinux.kpart-6.1.51-stb-mt8+ of=/dev/mmcblk0p2 bs=1M status=progress
+    sudo tar -xzvf ~/6.4.7-stb-mt8+.tar.gz
+    sudo rm -rf 6.4.7-stb-mt8+.tar.gz
+    sudo dd if=/boot/vmlinux.kpart-6.4.7-stb-mt8+ of=/dev/mmcblk0p1 bs=1M status=progress
+    sudo dd if=/boot/vmlinux.kpart-6.4.7-stb-mt8+ of=/dev/mmcblk0p2 bs=1M status=progress
 fi
 
 echo "Installing Deepin Sound Theme"
