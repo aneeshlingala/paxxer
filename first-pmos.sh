2d8e76a6aaf2a76b0ee97c79fa689fd93a61061f
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-07-06 19:43:41 -0700

    postmarketOS support

---
+++ first-pmos.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+echo "Setting variables..."
+PAXXERDIR=$PWD
+CUR_HOSTNAME=$(cat /etc/hostname)
+KERNEL=$(uname -r)
+ARCH=$(uname -m)
+
+sudo apk add curl bash alpine-conf git
+clear
+echo ""
+echo "Announcements from Paxxer Developers:"
+echo ""
+curl -s https://raw.githubusercontent.com/aneeshlingala/paxxer/paxxer/announcements | cat
+echo ""
+echo "Press the spacebar to exit announcements..."
+read -r -s -d ' '
+echo ""
+
+echo "Running a few checks..."
+
+if [[ $EUID -eq 0 ]]; then
+echo "Error: Do not run this script as root!" 1>&2
+echo "Solution: Run this script as a normal user without sudo."
+exit
+fi
+
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+  echo "You are online, continuing..."
+  echo ""
+else
+  echo "Error: You are offline."
+  echo ""
+  echo "Press any key to launch the Network Connection Wizard..."
+  read -s -n 1
+  echo ""
+  echo "Pressed a key, launching the Network Connection Wizard..."
+  nmtui
+fi
+
+if [[ -f "$PWD/second.sh" ]]; then
+    echo "Script is running from the root directory of the repository, continuing..."
+else
+    echo "Error: Please run this script from the Paxxer repository."
+    exit
+fi
+
+if [[ -f "/etc/paxxer-successful" ]]; then
+    echo "Error: Paxxer was launched before on this system."
+    exit
+else
+    echo "Paxxer has not run before, continuing..."
+fi
+
+if [[ "$ARCH" == "x86_64" ]]; then
+    echo "Architecture is x64, continuing..."
+else
+    if [[ "$ARCH" == "aarch64" ]]; then
+        echo "Architecture is aarch64, continuing..."
+    else
+        echo "Error: Paxxer does not work on $(echo $ARCH) architecture."
+    fi
+fi
+
+echo "PaxxerPmOS, a setup tool to setup my PostmarketOS system, to my liking."
+echo "Version: 2024.07.06"
+
+echo "Adding user Aneesh..."
+sudo adduser aneesh
+sudo mkdir /home/aneesh
+sudo usermod -a -G wheel aneesh
+echo "Setting password for Aneesh user account..."
+sudo passwd aneesh
+echo "Setting root password..."
+sudo passwd
+
+
+if [[ "$ARCH" == "aarch64" ]]; then
+    echo "Setting Hostname..."
+    sudo setup-hostname -n kappa
+fi
+
+cd ~
+sudo git clone https://github.com/vinceliuice/Graphite-gtk-theme
+cd Graphite-gtk-theme
+sudo apk add sassc
+sudo bash install.sh -t teal -c dark --tweaks nord 
+cd ~
+sudo git clone https://github.com/SylEleuth/gruvbox-plus-icon-pack
+cd gruvbox-plus-icon-pack
+sudo cp -r Gruvbox-Plus-Dark /usr/share/icons/
+cd ~
+sudo git clone https://github.com/yeyushengfan258/Afterglow-Cursors
+cd Afterglow-Cursors
+sudo mkdir /usr/share/icons
+sudo cp -r dist /usr/share/icons/Afterglow-cursors
+cd ~
+sudo rm -rf Afterglow-Cursors
+echo "Setting timezone to America/Los_Angeles"
+sudo setup-timezone -z America/Los_Angeles
+echo "Setting up Flatpak and installing GTKCord4 (Discord Client for Linux, supporting ARM64, built on GTK4), Brave Browser, and Minecraft Pi..."
+sudo apk add flatpak
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub so.libdb.gtkcord4 -y
+flatpak install flathub com.thebrokenrail.MCPIReborn
+echo "Upgrading System..."
+sudo apk update
+sudo apk upgrade
+echo "Cleaning up..."
+sudo rm -rf /var/cache/apk/*
+echo "Installing KDE Desktop..."
+cd ~
+sudo apk add postmarketos-ui-plasma-desktop gwenview ark atril telegram-desktop plasma-systemmonitor cups kcalc -y
+echo "Installing KDE Themes..."
+cd ~
+sudo git clone https://github.com/vinceliuice/Graphite-kde-theme
+cd Graphite-kde-theme
+sudo bash install.sh
+cd ~
+echo "Installing Visual Studio Code..."
+sudo apk add code-oss
+sudo mkdir /home/aneesh/VSCode
+sudo chown aneesh:aneesh /home/aneesh/VSCode
+echo "Installing SDDM and setting it up..."
+sudo apk add qt6-qtbase qt5-qtquickcontrols2 qt5-qtgraphicaleffects
+sudo rc-update add sddm
+sudo apk add plasma-nm
+sudo mkdir /usr/share/sddm/themes/
+cd /usr/share/sddm/themes/
+sudo git clone https://github.com/erickotato/sddm-slice slice
+echo "Current=slice" | sudo tee -a /etc/sddm.conf
+echo "CursorTheme=Afterglow-cursors" | sudo tee -a /etc/sddm.conf
+echo "Adding Alpine Linux Backgrounds..."
+sudo mkdir /usr/share/backgrounds
+sudo mkdir /usr/share/backgrounds/bg
+sudo cp $PAXXERDIR/bg.jpg /usr/share/backgrounds/bg/
+cd ~
+
+echo "Setting default shell as fish..."
+sudo chsh --shell /usr/bin/fish aneesh
+echo "Copying files for second.sh..."
+sudo mkdir /home/aneesh/paxxer
+sudo cp $PAXXERDIR/second.sh /home/aneesh/paxxer
+sudo chmod +x /home/aneesh/paxxer/second.sh
+sudo touch /etc/paxxer-first-done
+
+if [[ "$ARCH" == "x86_64" ]]; then
+    echo "Setting Hostname..."
+    sudo setup-hostname -n terra
+fi
+
+sudo rm -rf ~/gruvbox-plus-icon-pack ~/Graphite-gtk-theme
+echo "After rebooting, run the second-pmos.sh script located in /home/aneesh/paxxer with bash."
+echo "NOTE: After rebooting, login as user aneesh, or the script will break."
+echo ""
+echo "Press any key to reboot..."
+read -s -n 1
+echo ""
+echo "Pressed a key, rebooting..."
+sudo reboot
---
+++ second-pmos.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+if [[ $EUID -eq 0 ]]; then
+  echo "This script should not be run as root" 1>&2
+  echo "Solution: Run this script as a normal user without sudo." 1>&2
+  exit
+fi
+
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+  echo "You are online, continuing..."
+  echo ""
+else
+  echo "Error: You are offline."
+  echo ""
+  echo "Press any key to launch the Network Connection Wizard..."
+  read -s -n 1
+  echo ""
+  echo "Pressed a key, launching the Network Connection Wizard..."
+  nmtui
+fi
+
+if [ "$(whoami)" != "aneesh" ]; then
+        echo "Error: Please reboot and run the script as user aneesh."
+        echo "Not rebooting but instead logging out and logging in as aneesh will cause the script to break."
+        echo "Rebooting in 5 seconds..."
+        sleep 5
+        sudo reboot
+        exit
+fi
+
+if [[ -f "/etc/paxxer-first-done" ]]; then
+    echo "First-pmos.sh has been run, good."
+    sudo rm -rf /etc/paxxer-first-done
+else
+    echo "Error: Please run first-pmos.sh, then second-pmos.sh."
+    exit
+fi
+
+sudo chown -R aneesh:aneesh /home/aneesh
+cd /home/aneesh/paxxer
+
+echo "Deleting user linux..."
+sudo userdel linux
+sudo rm -rf /home/linux
+echo "Setting up greeting for fish..."
+cd ~
+echo "echo Welcome to PostmarketOS!" > ~/.config/fish/config.fish
+fish -c "set -U fish_greeting "🐟" "
+echo "Adding Minecraft Pi Mods..."
+sudo mkdir /home/aneesh/.minecraft-pi/
+sudo mkdir /home/aneesh/.minecraft-pi/mods
+sudo chown -R aneesh:aneesh /home/aneesh/.minecraft-pi
+cd /home/aneesh/.minecraft-pi/mods
+sudo apk add wget
+wget "https://cdn.discordapp.com/attachments/740287938453045401/1078558046613143562/libcake.so"
+wget "https://github.com/Bigjango13/MCPI-Mods/releases/download/v1.0.2/libexpanded-creative.so"
+wget "https://cdn.discordapp.com/attachments/889201475362893844/1003050259712331796/libNoReactorMessage.so"
+wget "https://cdn.discordapp.com/attachments/1034896064240689192/1053892162284163273/libspawnEgg.so"
+wget "https://github.com/NikZapp/mcpi-block-shenanigans/releases/download/v1.0/libmcpiblocks.so"
+wget "https://github.com/NikZapp/mcpi-better-grass-mod/releases/download/v1.0/libniksbettergrass.so"
+
+echo "Theming GTK-4 and Flatpak Apps..."
+rm -rf ~/.config/gtk-4.0/gtk.css
+rm -rf ~/.config/gtk-4.0/gtk-dark.css
+rm -rf ~/.config/gtk-4.0/assets
+rm -rf ~/.config/assets
+mkdir ~/.config/gtk-4.0
+ln -s /usr/share/themes/Graphite-teal-Dark-nord/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
+ln -s /usr/share/themes/Graphite-teal-Dark-nord/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
+ln -s /usr/share/themes/Graphite-teal-Dark-nord/gtk-4.0/assets ~/.config/gtk-4.0/assets
+ln -s /usr/share/themes/Graphite-teal-Dark-nord/assets ~/.config/assets
+echo "GTK_THEME=Graphite-teal-Dark-nord" | sudo tee -a /etc/environment
+sudo cp -r /usr/share/themes/Graphite-teal-Dark-nord ~/.themes/
+sudo flatpak override --filesystem=$HOME/.themes
+sudo flatpak override --env=GTK_THEME=Graphite-teal-Dark-nord
+lookandfeeltool -a Graphite-nord-dark
+cd ~
+cd /home/aneesh/paxxer
+uname -r | sudo tee -a /etc/paxxer-kernel
+echo "Downloading extra needed files..."
+sudo wget https://raw.githubusercontent.com/aneeshlingala/paxxer/paxxer/.conkyrc
+sudo wget https://raw.githubusercontent.com/aneeshlingala/paxxer/paxxer/conky-startup.desktop
+cd ~
+
+if [[ "$ARCH" == "aarch64" ]]; then
+   echo "Setting up Conky..."
+   sudo apk add conky
+   cd /home/aneesh/paxxer
+   cp -r .conkyrc ~
+   mkdir ~/.config
+   mkdir ~/.config/autostart
+   cp -r conky-startup.desktop ~/.config/autostart/
+fi
+
+sleep 11
+sudo touch /etc/paxxer-successful
+cd ~
+sudo rm -rf /home/aneesh/paxxer
+echo "PLEASE READ below:"
+echo "Install harleen oh-my-fish theme after installation with omf install harleen."
+echo "Then, reboot the computer with the command systemctl reboot."
+echo "Press any key to install oh-my-fish..."
+read -s -n 1
+echo ""
+echo "Pressed a key, installing oh-my-fish..."
+cd ~
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
