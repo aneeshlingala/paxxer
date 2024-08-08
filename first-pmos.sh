34430e7af2e55f8b7e99a78e4bcd4e8de6b35772
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-08-07 18:12:29 -0700

    Update first-pmos.sh

--- first-pmos.sh
+++ first-pmos.sh
@@ -63,7 +63,7 @@ else
 fi
 
 echo "PaxxerPmOS, a setup tool to setup my PostmarketOS system, to my liking."
-echo "Version: 2024.07.18"
+echo "Version: 2024.08.07"
 
 echo "Adding user Aneesh..."
 sudo adduser aneesh
