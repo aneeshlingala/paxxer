60043280c8c0cc10e7dfa8c6824058f694405421
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-08-09 13:16:01 -0700

    Update first-rpi.sh

--- first-rpi.sh
+++ first-rpi.sh
@@ -109,7 +109,7 @@ sudo bash install.sh -t teal -c dark --tweaks nord
 cd ~
 sudo git clone https://github.com/SylEleuth/gruvbox-plus-icon-pack
 cd gruvbox-plus-icon-pack
-sudo cp -r Gruvbox-Plus-Dark /usr/share/icons/
+sudo cp -r Gruvbox* /usr/share/icons/
 cd ~
 sudo git clone https://github.com/yeyushengfan258/Afterglow-Cursors
 cd Afterglow-Cursors
