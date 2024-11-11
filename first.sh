8c102fc1e5ea7defd6cb181107bf4e32f2703c53
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-11-11 13:05:21 -0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -103,12 +103,13 @@ if [[ "$ARCH" == "aarch64" ]]; then
     sudo sed -i "s/$CUR_HOSTNAME/kappa/g" /etc/hosts
     sudo sed -i "s/$CUR_HOSTNAME/kappa/g" /etc/hostname
     echo "Updating kernel from $(echo $KERNEL) to 6.6.9-stb-mt8+"
-    sudo rm -rf /boot/*
-    sudo rm -rf /lib/modules/*
     cd ~
     sudo wget "https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/6.6.9-stb-mt8%2B/6.6.9-stb-mt8+.tar.gz"
-    cd /
     sudo tar -xzvf ~/6.6.9-stb-mt8+.tar.gz
+    sudo cp -r lib/modules/* /lib/modules/
+    sudo rm -rf /boot/*
+    sudo cp -r boot/* /boot/
+    sudo cp -r 
     sudo rm -rf 6.6.9-stb-mt8+.tar.gz
     sudo dd if=/boot/vmlinux.kpart-6.6.9-stb-mt8+ of=/dev/mmcblk0p1 bs=1M status=progress
     sudo dd if=/boot/vmlinux.kpart-6.6.9-stb-mt8+ of=/dev/mmcblk0p2 bs=1M status=progress
