f3818b3fd66e5206310d87b36fcf435896f8177d
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-12-02 02:51:47 +0000

    Media Writer

--- second-pmos.sh
+++ second-pmos.sh
@@ -60,7 +60,7 @@ wget "https://cdn.discordapp.com/attachments/889201475362893844/1003050259712331
 wget "https://cdn.discordapp.com/attachments/1034896064240689192/1053892162284163273/libspawnEgg.so"
 wget "https://github.com/NikZapp/mcpi-block-shenanigans/releases/download/v1.0/libmcpiblocks.so"
 wget "https://github.com/NikZapp/mcpi-better-grass-mod/releases/download/v1.0/libniksbettergrass.so"
-echo "Setting up Flatpak and installing GoofCord (Discord Client for Linux, supporting ARM64 & x86_64), VSCode, Brave Browser, and Minecraft Pi..."
+echo "Setting up Flatpak and installing GoofCord (Discord Client for Linux, supporting ARM64 & x86_64), VSCode, Brave Browser, Fedora Media Writer, and Minecraft Pi..."
 echo y | sudo apk add flatpak xdg-desktop-portal-kde
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user flathub io.github.milkshiift.GoofCord -y
@@ -68,6 +68,7 @@ flatpak install --user flathub com.thebrokenrail.MCPIReborn -y
 flatpak install --user flathub io.gitlab.librewolf-community -y
 flatpak install --user flathub com.visualstudio.code -y
 flatpak install --user flathub com.brave.Browser -y
+flatpak install --user flathub org.fedoraproject.MediaWriter -y
 sudo mkdir /home/aneesh/VSCode
 sudo chown aneesh:aneesh /home/aneesh/VSCode
 echo "Theming GTK-4 and Flatpak Apps..."
