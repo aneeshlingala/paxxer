8caf5109058d74227437367da99cc75eb2762f41
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2025-02-09 13:44:37 -0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -151,7 +151,7 @@ sudo apt install lsb-release
 curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
 echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
 sudo apt update
-sudo apt install prismlauncher openjdk-17-jdk
+sudo apt install prismlauncher openjdk-17-jdk -y
 
 if [[ "$ARCH" == "aarch64" ]]; then
     echo "Installing Java..."
