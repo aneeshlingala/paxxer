404bf1498314df5bdf46e13c168228fac55757d4
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2025-02-28 17:25:21 -0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -70,7 +70,7 @@ then
 fi
 
 echo "PaxxerDeb, a setup tool to setup my Debian system, to my liking."
-echo "Version: 2025.02.22"
+echo "Version: 2025.02.28"
 
 if [[ -f "/scripts/extend-rootfs.sh" ]]; then
     echo "The script extend-rootfs.sh exists, running it..."
@@ -190,6 +190,11 @@ if [[ "$ARCH" == "x86_64" ]]; then
     sudo mkdir /home/aneesh/Desktop/
     sudo cp $PAXXERDIR/mcreator.desktop /home/aneesh/Desktop/
     echo "Run /home/aneesh/MCreator/mcreator.sh for MCreator!"
+    echo "Installing Lutris..."
+    echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+    wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
+    sudo apt update
+    sudo apt install lutris -y
 fi
 
 echo "Upgrading System..."
